a4ecf9252a2b78e3fa0f20236f936957fbe8a355
yossifSale7 <121363323+yossifSale7@users.noreply.github.com>
2023-02-09 20:15:16 +0200

    Add files via upload

---
+++ mail.php
@@ -0,0 +1,38 @@
+<?php
+//Import PHPMailer classes into the global namespace
+//These must be at the top of your script, not inside a function
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+
+//Load Composer's autoloader
+require 'mailer/autoload.php';
+
+//Create an instance; passing `true` enables exceptions
+if(isset($_POST['send'])){
+    $name = htmlentities($_POST['name']);
+    $email = htmlentities($_POST['email']);
+    $subject = htmlentities($_POST['subject']);
+    $adress = htmlentities($_POST['address']);
+    $message = htmlentities($_POST['message']);
+
+    $mail = new PHPMailer(true);
+    $mail->isSMTP();
+    $mail->Host = 'smtp.gmail.com';
+    $mail->SMTPAuth = true;
+    $mail->Username = 'Www.IcleanAEY@gmail.com';
+    $mail->Password = 'xmzlzgwqlqnprigc';
+    $mail->Port = 465;
+    $mail->SMTPSecure = 'ssl';
+    $mail->isHTML(true);
+    $mail->setFrom($email, $name);
+    $mail->addAddress('Www.IcleanAEY@gmail.com');
+    $mail->Subject = ("$email ($subject)");
+    $mail->Body = $message;
+    $mail->Body = $address;
+    $mail->send();
+
+    header("Location: ./response.html");
+}
+
+?>
+
